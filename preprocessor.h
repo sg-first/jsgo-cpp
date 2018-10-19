75161a5fab4d618be487f86b16bdf9479ef621d7
Mingkai Zhou <524019351@qq.com>
2018-10-19 10:22:10 +0800

    修改注释的BUG

--- preprocessor.h
+++ preprocessor.h
@@ -9,6 +9,6 @@ public:
     static String prepro(String str, String &newcode);
 
 private:
-    static void deleteComment(String &str);
+    static void deleteMulComment(String &str);
     static void deleteSpace(String &str); //把几个连一起的空格变成一个空格
 };
